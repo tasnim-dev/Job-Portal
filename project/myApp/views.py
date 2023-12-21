718e8db9524d44d290520fdf0ac499d76c343683
tasnim-dev <107361707+tasnim-dev@users.noreply.github.com>
2023-12-21 23:41:03 +0600

    signup and login page

--- project/myApp/views.py
+++ project/myApp/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+<<<<<<< HEAD
 def loginPage(request):
     return render (request, 'login.html')
+=======
+def signupPage(request):
+    return render (request, 'signup.html')
+>>>>>>> e718793 (signup and login page)
