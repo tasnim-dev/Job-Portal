6ced0b19ea607d8dd4a99977e2cb5048609566f8
Mir Monoarul Alam <14123130+anik-cse@users.noreply.github.com>
2023-12-28 15:01:39 +0600

    register page

--- project/myApp/views.py
+++ project/myApp/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-<<<<<<< HEAD
 def loginPage(request):
     return render (request, 'login.html')
-=======
+
 def signupPage(request):
-    return render (request, 'signup.html')
->>>>>>> e718793 (signup and login page)
+    return render (request, 'register.html')
