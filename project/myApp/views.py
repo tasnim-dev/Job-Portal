e99b07a07ae722cbe4046ba1d5b481671fdb9432
tasnim-dev <107361707+tasnim-dev@users.noreply.github.com>
2023-12-28 20:04:07 +0600

    templates

--- project/myApp/views.py
+++ project/myApp/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from django.contrib.auth import authenticate,login,logout
+from .models import *
 
 # Create your views here.
 
 def loginPage(request):
     return render (request, 'login.html')
 
+#Signup Page
 def signupPage(request):
+    # if request.method == "POST":
+
+
     return render (request, 'register.html')
+
+#Dashboard Page
+def dashboardPage(request):
+    user = request.user
+
+    if user.is_authenticated:
+        if user.user_type == 'recruiter':
+            Template_name = 'recruiter/dashboard.html'
+        
+        elif user.user_type == 'jobseeker' :
+            Template_name = 'jobseeker/dashboard.html'
+
+        else:
+            return HttpResponse ('invalid user')
+
+    else:
+        return HttpResponse('User is not authenticate')
+    return render(request, Template_name)
