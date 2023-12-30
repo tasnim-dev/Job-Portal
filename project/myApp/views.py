5deebc2e77c0cfd891b4fb7f023bc4381ce3e641
tasnim-dev <107361707+tasnim-dev@users.noreply.github.com>
2023-12-30 12:22:46 +0600

    signup

--- project/myApp/views.py
+++ project/myApp/views.py
@@ -1,15 +1,41 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth import authenticate,login,logout
 from .models import *
 
 # Create your views here.
 
 def loginPage(request):
+
+    if request.method == "POST":
+        user_name = request.POST.get('username')
+        user_pass = request.POST.get('password')
+
+        user = authenticate(request,username=user_name, password=user_pass)
+
+        if user:
+            login(request,user)
+            return redirect('dashboardPage')
+
     return render (request, 'login.html')
 
 #Signup Page
 def signupPage(request):
-    # if request.method == "POST":
+    if request.method == "POST":
+
+        uname=request.POST.get('username')
+        d_name=request.POST.get('displayname')
+        myemail=request.POST.get('email')
+        mypass=request.POST.get('password')
+        user_type=request.POST.get('userType')
+
+        user = customUser.objects.create_user(username=uname,password=mypass)
+        user.Display_name =d_name
+        user.email=myemail
+        user.user_type=user_type
+
+        user.save()
+        return render (request, 'register.html')
+
 
 
     return render (request, 'register.html')
